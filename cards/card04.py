9a381030ab3f6d7118518c53c76b305025294ade
paolo.bertin <paolo.bertin.62@gmail.com>
2023-12-15 14:03:25 +0100

    Aggiunta scheda 4

--- cards/card04.py
+++ cards/card04.py
@@ -9,6 +9,21 @@ class Card04:
 
     def initialize(self, content, i) -> int:
         """Inizializzazione"""
+
+        i += 1
+        line_card = content[i].split(",")
+        for parameters in line_card:
+            token = parameters.split("=")
+            var = token[0].strip()
+            val = token[1].strip()
+            match var:
+                case "idhist1":
+                    self.idhist[0] = int(val)
+                case "idhist2":
+                    self.idhist[1] = int(val)
+                case "idhist3":
+                    self.idhist[2] = int(val)
+
         return i
 
     def to_string(self) -> list[str]:
